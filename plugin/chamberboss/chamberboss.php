b733c93b0e17cfcee585802147bceb0be452447b
lancesmithcc <lancesmithcc@gmail.com>
2025-07-26 14:44:27 -0700

    fatal fix again

--- plugin/chamberboss/chamberboss.php
+++ plugin/chamberboss/chamberboss.php
@@ -28,8 +28,7 @@ define('CHAMBERBOSS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('CHAMBERBOSS_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('CHAMBERBOSS_PLUGIN_BASENAME', plugin_basename(__FILE__));
 
-require_once CHAMBERBOSS_PLUGIN_DIR . 'includes/Core/Database.php';
-require_once CHAMBERBOSS_PLUGIN_DIR . 'includes/Core/PostTypes.php';
+
 
 // Autoloader for plugin classes
 spl_autoload_register(function ($class) {
